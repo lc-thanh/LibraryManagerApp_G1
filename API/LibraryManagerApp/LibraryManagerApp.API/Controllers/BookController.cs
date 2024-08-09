0e77a6d4dccae493c31fd4295e3bc03f1d230513
Thanh <thanhcong02052003@gmail.com>
2024-08-09 22:42:53 +0700

    create bookcontroller

---
+++ API/LibraryManagerApp/LibraryManagerApp.API/Controllers/BookController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagerApp.API.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
