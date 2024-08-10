bce7dd99e6ee7b52373a4f8a7ca8d9dd87a18d2c
Le Anh Van <lea91441@gmail.com>
2024-08-10 23:56:48 +0700

    AuthorCreateModel

---
+++ API/LibraryManagerApp/LibraryManagerApp.Data/Dto/AuthorCreateModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagerApp.Data.Dto
+{
+    public class AuthorCreateModel
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
