81e0e22e30e6c6e7870ab5d511259ede3c556711
Thanh <thanhcong02052003@gmail.com>
2024-08-12 14:12:43 +0700

    loanManagement

---
+++ API/LibraryManagerApp/LibraryManagerApp.Data/Dto/LoanCreateModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagerApp.Data.Dto
+{
+    public class LoanCreateModel
+    {
+        [Required]
+        public Guid MemberId { get; set; }
+        [Required]
+        public List<Guid> BookId { get; set; }
+        [Required]
+        public DateTime LoanDate { get; set; } = DateTime.Now;
+
+        [Required]
+        public DateTime DueDate { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
--- API/LibraryManagerApp/LibraryManagerApp.Data/Repository/LoanRepository.cs
+++ API/LibraryManagerApp/LibraryManagerApp.Data/Repository/LoanRepository.cs
@@ -1,6 +1,9 @@
 ﻿using LibraryManagerApp.Data.Data;
 using LibraryManagerApp.Data.Interfaces;
 using LibraryManagerApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System;
 
 namespace LibraryManagerApp.Data.Repository
 {
@@ -8,6 +11,41 @@ namespace LibraryManagerApp.Data.Repository
     {
         public LoanRepository(LibraryManagerAppDbContext context) : base(context)
         {
+        }
+        public async Task CreateLoanAsync(Loan loan)
+        {
+            _context.Loans.Add(loan);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Loan> GetLoanByIdAsync(Guid loanId)
+        {
+            return await _context.Loans
+            .Include(l => l.LoanDetails)
+            .ThenInclude(ld => ld.Book)
+            .FirstOrDefaultAsync(l => l.Id == loanId);
+        }
+
+        public async Task ReturnBooksAsync(Guid loanId)
+        {
+            var loan = await GetLoanByIdAsync(loanId);
+            if (loan != null)
+            {
+                loan.ReturnedDate = DateTime.Now;
+                _context.Loans.Update(loan);
+
+                // Optionally, update book quantities if needed
+                foreach (var detail in loan.LoanDetails)
+                {
+                    var book = await _context.Books.FindAsync(detail.BookId);
+                    if (book != null)
+                    {
+                        book.AvailableQuantity++;
+                    }
+                }
+
+                await _context.SaveChangesAsync();
+            }
         }
     }
 }
