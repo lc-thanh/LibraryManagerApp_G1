80efc4eec1c0b3d1d6cbbb0bc19a44ba1a05d2b7
=LC Thanh <lcthanh.htvn@gmail.com>
2024-08-15 00:50:41 +0700

    Complete Books manager and Update Login/Signup, fix JWT Authentication

--- API/LibraryManagerApp/LibraryManagerApp.API/Controllers/BookController.cs
+++ API/LibraryManagerApp/LibraryManagerApp.API/Controllers/BookController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 using LibraryManagerApp.Data.Dto;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
+using LibraryManagerApp.Data.Pagination;
+using System.Linq.Expressions;
 
 namespace LibraryManagerApp.API.Controllers
 {
@@ -22,21 +24,19 @@ namespace LibraryManagerApp.API.Controllers
 
         [HttpGet]
         public async Task<IActionResult> GetAllBooks(
-            [FromForm] string searchString = "",
-            [FromForm] string orderBy = "",
-            [FromForm] string publishedYearRange = "",
-            [FromForm] List<Guid>? authorIds = null,
-            [FromForm] List<Guid>? categoryIds = null,
-            [FromForm] int pageNumber = 1,
-            [FromForm] int pageSize = 10)
+            [FromQuery] string? searchString = "",
+            [FromQuery] string? orderBy = "",
+            [FromQuery] string? publishedYearRange = "",
+            [FromQuery] List<Guid>? authorIds = null,
+            [FromQuery] List<Guid>? categoryIds = null,
+            [FromQuery] int pageNumber = 1,
+            [FromQuery] int pageSize = 10)
         {
             var booksQuery = _unitOfWork.BookRepository.GetAllInforsQuery();
 
-            
-
             var baseUrl = $"{Request.Scheme}://{Request.Host}";
 
-            var bookViewModels = await booksQuery.Select(b => new BookViewModel
+            var bookViewModels = booksQuery.Select(b => new BookViewModel
             {
                 Title = b.Title,
                 Publisher = b.Publisher,
@@ -53,9 +53,111 @@ namespace LibraryManagerApp.API.Controllers
                 BookShelfId = b.BookShelfId,
                 BookShelfName = b.BookShelf.Name,
                 CreatedOn = b.CreatedOn,
-            }).ToListAsync();
+            });
 
-            return Ok(bookViewModels);
+            List<Expression<Func<BookViewModel, bool>>> filterList = new List<Expression<Func<BookViewModel, bool>>>();
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                filterList.Add(b =>
+                    b.Title.ToLower().Contains(searchString.ToLower()) ||
+                    ((!string.IsNullOrEmpty(b.Publisher)) ? (b.Publisher.ToLower().Contains(searchString.ToLower())) : false) ||
+                    ((!string.IsNullOrEmpty(b.BookShelfName)) ? (b.BookShelfName.ToLower().Contains(searchString.ToLower())) : false)
+                );
+            }
+            if (!string.IsNullOrEmpty(publishedYearRange))
+            {
+                int yearStart;
+                int.TryParse(publishedYearRange.Split('-')[0], out yearStart);
+
+                int yearEnd;
+                int.TryParse(publishedYearRange.Split('-')[1], out yearEnd);
+
+                filterList.Add(b => yearStart <= b.PublishedYear && b.PublishedYear <= yearEnd);
+            }
+            if (authorIds != null)
+            {
+                if (authorIds.Count() > 0)
+                {
+                    filterList.Add(b => (b.AuthorId != null) ? authorIds.Contains((Guid) b.AuthorId) : false);
+                }
+            }
+            if (categoryIds != null)
+            {
+                if (categoryIds.Count() > 0)
+                {
+                    filterList.Add(b => (b.CategoryId != null) ? categoryIds.Contains((Guid) b.CategoryId) : false);
+                }
+            }
+
+            Func<IQueryable<BookViewModel>, IOrderedQueryable<BookViewModel>>? orderFunc = null;
+
+            // Mặc định là xếp theo ngày tạo
+            orderFunc = query => query.OrderByDescending(b => b.CreatedOn);
+
+            if (!string.IsNullOrEmpty(orderBy))
+            {
+                string orderByString = orderBy.Split('-')[0];
+                string ascOrDesc = orderBy.Split("-")[1];
+
+                switch (orderByString)
+                {
+                    case "Title":
+                        if (ascOrDesc.Equals("Asc"))
+                            orderFunc = query => query.OrderBy(b => b.Title);
+                        else
+                            orderFunc = query => query.OrderByDescending(b => b.Title);
+                        break;
+
+                    case "Quantity":
+                        if (ascOrDesc.Equals("Asc"))
+                            orderFunc = query => query.OrderBy(b => b.Quantity);
+                        else
+                            orderFunc = query => query.OrderByDescending(b => b.Quantity);
+                        break;
+
+                    case "AvailableQuantity":
+                        if (ascOrDesc.Equals("Asc"))
+                            orderFunc = query => query.OrderBy(b => b.AvailableQuantity);
+                        else
+                            orderFunc = query => query.OrderByDescending(b => b.AvailableQuantity);
+                        break;
+
+                    case "TotalPages":
+                        if (ascOrDesc.Equals("Asc"))
+                            orderFunc = query => query.OrderBy(b => b.TotalPages);
+                        else
+                            orderFunc = query => query.OrderByDescending(b => b.TotalPages);
+                        break;
+
+                    case "PublishedYear":
+                        if (ascOrDesc.Equals("Asc"))
+                            orderFunc = query => query.OrderBy(b => b.PublishedYear);
+                        else
+                            orderFunc = query => query.OrderByDescending(b => b.PublishedYear);
+                        break;
+
+                    case "CreatedOn":
+                        if (ascOrDesc.Equals("Asc"))
+                            orderFunc = query => query.OrderBy(b => b.CreatedOn);
+                        else
+                            orderFunc = query => query.OrderByDescending(b => b.CreatedOn);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            PaginatedResult<BookViewModel> paginatedBooks = await _unitOfWork.BaseRepository<BookViewModel>().GetPaginatedAsync(
+                bookViewModels, 
+                filterList,
+                orderFunc,
+                "",
+                pageNumber,
+                pageSize
+            );
+
+            return Ok(paginatedBooks);
         }
 
         [HttpPost]
---
+++ Website/AuthPages/script.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+    var panelOne = $('.form-panel.two').height(),
+      panelTwo = $('.form-panel.two')[0].scrollHeight;
+  
+    $('.form-panel.two').not('.form-panel.two.active').on('click', function(e) {
+      e.preventDefault();
+  
+      $('.form-toggle').addClass('visible');
+      $('.form-panel.one').addClass('hidden');
+      $('.form-panel.two').addClass('active');
+      $('.form').animate({
+        'height': panelTwo
+      }, 200);
+    });
+  
+    $('.form-toggle').on('click', function(e) {
+      e.preventDefault();
+      $(this).removeClass('visible');
+      $('.form-panel.one').removeClass('hidden');
+      $('.form-panel.two').removeClass('active');
+      $('.form').animate({
+        'height': panelOne
+      }, 200);
+    });
+
+    $('#loginForm').on('submit', function(e) {
+        e.preventDefault();
+
+        var loginData = {
+            email: $('#loginEmail').val(),
+            password: $('#loginPassword').val()
+        };
+
+        $.ajax({
+            url: 'https://localhost:44396/api/v1/Auths/login', // Địa chỉ API để xử lý đăng nhập
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(loginData),
+            success: function (response) {
+                // Xử lý khi đăng nhập thành công
+                console.log('Login successful:', response);
+
+                // Lưu Access Token và Refresh Token vào localStorage
+                localStorage.setItem('accessToken', response.token);
+                localStorage.setItem('refreshToken', response.refreshToken);
+
+                // Chuyển hướng đến trang khác hoặc thực hiện các hành động khác
+                window.location.href = '../QuanLySach/QLSach.html';
+            },
+            error: function (xhr, status, error) {
+                // Xử lý khi đăng nhập thất bại
+                console.error('Login failed:', error);
+                alert("Đăng nhập thất bại")
+            }
+        });
+    })
+  });
